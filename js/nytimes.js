ccf38b78b68e1e960d83aab17bf7af34d5b23713
Zeleng Zhuang <zhuangzeleng19920731@gmail.com>
2017-01-21 02:58:12 -0500

    a small modification

--- js/nytimes.js
+++ js/nytimes.js
@@ -1,8 +1,8 @@
 var apikey = "4ac14040bca040e19edfffcbad7a7dbb";
 
 var formNYTimesUrl = function (name) {
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    url += $.param({
         'api-key': apikey,
         'q': name
     });
